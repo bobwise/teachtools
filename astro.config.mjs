625c2aaca3e4d63af0fb783f8a8754ed4fbde385
Bob <bobwise001@gmail.com>
2026-04-01 10:03:36 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bobwise.github.io/teachtools/',
+    site: 'https://bobwise.github.io',
     base: '/teachtools',
 });
