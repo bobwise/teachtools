938f581573f37c8981f652ebb38771c7d3d51848
Bob <bobwise001@gmail.com>
2026-04-01 23:50:52 -0400

    d

---
+++ src/constants/default-names.js
@@ -0,0 +1,3 @@
+export const DEFAULT_NAMES = ['Dracula', 'Wendy Darling', 'Mulan', 'Robin Hood', 'Dorothy Gale', 'Beowulf', 'Sherlock Holmes', 'Esmeralda', 'Frankenstein\'s Monster'];
+
+export const DEFAULT_LEADER_NAMES = ['Athena', 'Loki', 'Freya', 'Persephone', 'Hera'];
